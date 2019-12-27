17cbcee81d7591e4dd3355b7e743cdbd2f3184c4
Brad Nelson <bjnelson@stanford.edu>
2019-12-27 15:41:10 -0800

    getitem in dense array

--- bats/dense.cpp
+++ bats/dense.cpp
@@ -11,15 +11,14 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(dense, m) {
 
-	// column major - side effect is that the matrix makes a transpose
 	py::class_< Matrix<double> >(m, "DenseDoubleMatrix", py::buffer_protocol())
 		.def(py::init<>())
 		.def(py::init<size_t, size_t>())
 		.def("nrow", &Matrix<double>::nrow)
 		.def("ncol", &Matrix<double>::ncol)
-		.def("__getitem__", py::overload_cast<int, int>(&Matrix<double>::operator(), py::const_))
 		.def("__call__", py::overload_cast<int, int>(&Matrix<double>::operator(), py::const_))
 		.def("__init__", [](Matrix<double> &m, py::buffer b) {
+			// WARNING: this has the side-effect of transposing the matrix
 
 	        /* Request a buffer descriptor from Python */
 	        py::buffer_info info = b.request();
@@ -45,9 +44,10 @@ PYBIND11_MODULE(dense, m) {
 	            { sizeof(double),                        /* Strides (in bytes) for each index */
 	              sizeof(double) * m.nrow()}
 	        );
-	    });
+	    })
+		.def("__getitem__", [](const Matrix<double> &m, std::vector<int> &inds) {return m(inds[0], inds[1]); } );
+		// .def("__getitem__", py::overload_cast<int, int>(&Matrix<double>::operator(), py::const_))
 		// .def("__getitem__", py::overload_cast<int, int>(&Matrix<double>::operator()))
-		//.def("__getitem__", [](const Matrix<double> &m, std::tuple<int, int> &inds) {return m(std::get<0>(inds), std::get<1>(inds)); } )
 		//.def("__setitem__", py::overload_cast<int, int>(&Matrix<double>::operator()))
 
 }
