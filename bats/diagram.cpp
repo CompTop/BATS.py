404f27644140b9ad0143dc083e84f600c88ca0ff
Brad Nelson <bradnelson@uchicago.edu>
2021-08-19 10:03:50 -0500

    quiver alg interfaces

--- bats/diagram.cpp
+++ bats/diagram.cpp
@@ -11,7 +11,7 @@ using namespace bats;
 .def(py::init<>())\
 .def(py::init<size_t, size_t>())\
 .def("set_node", (void (Diagram<NT, ET>::*)(size_t, NT&))(&Diagram<NT, ET>::set_node))\
-.def("set_edge", (void (Diagram<NT, ET>::*)(size_t, size_t, size_t, ET&))(&Diagram<NT, ET>::set_edge))\
+.def("set_edge", (void (Diagram<NT, ET>::*)(size_t, size_t, size_t, const ET&))(&Diagram<NT, ET>::set_edge))\
 .def("add_node", (size_t (Diagram<NT, ET>::*)(NT&))(&Diagram<NT, ET>::add_node))\
 .def("add_edge", (size_t (Diagram<NT, ET>::*)(size_t, size_t, ET&))(&Diagram<NT, ET>::add_edge))\
 .def("nnode", (size_t (Diagram<NT, ET>::*)())(&Diagram<NT, ET>::nnode))\
@@ -38,22 +38,29 @@ m.def("Chain", [](const SimplicialComplex &X, const SimplicialComplex &A, FT) {r
 m.def("Chain", [](const CubicalComplex &X, FT) {return ChainComplex<ColumnMatrix<SparseVector<FT, size_t>>>(X); });\
 m.def("Chain", [](const CellComplex &X, FT) {return ChainComplex<ColumnMatrix<SparseVector<FT, size_t>>>(X); });
 
+#define FlagInterface(T, name) py::class_<T>(m, name) \
+	.def(py::init<>());
+
+#define BarcodeInterface(NT, MT) \
+m.def("barcode", [](const Diagram<NT, MT>& D, size_t hdim){return barcode(D, hdim); });\
+m.def("barcode", [](const Diagram<NT, MT>& D, size_t hdim, flags::divide_conquer){return barcode(D, hdim, flags::divide_conquer()); });\
+m.def("barcode", [](const Diagram<NT, MT>& D, size_t hdim, flags::rightward){return barcode(D, hdim, flags::rightward()); });\
+m.def("barcode", [](const Diagram<NT, MT>& D, size_t hdim, flags::leftward){return barcode(D, hdim, flags::leftward()); });\
+m.def("barcode", [](const Diagram<NT, std::vector<MT>>& D){return barcode(D); });\
+m.def("barcode", [](const Diagram<NT, std::vector<MT>>& D, flags::divide_conquer){return barcode(D, flags::divide_conquer()); });\
+m.def("barcode", [](const Diagram<NT, std::vector<MT>>& D, flags::rightward){return barcode(D, flags::rightward()); });\
+m.def("barcode", [](const Diagram<NT, std::vector<MT>>& D, flags::leftward){return barcode(D, flags::leftward()); });
+
+
 
 #define HomFunctorInterface(MT, name) \
 m.def(name, [](const Diagram<ChainComplex<MT>, ChainMap<MT>> &D, size_t k) {return Hom(D, k);});\
 m.def(name, [](const Diagram<DGVectorSpace<MT>, DGLinearMap<MT>> &D, size_t k) {return Hom(D, k);});\
-m.def("barcode", [](const Diagram<ReducedChainComplex<MT>, MT>& D, size_t hdim){return barcode(D, hdim); });\
-m.def("barcode", [](const Diagram<ReducedDGVectorSpace<MT>, MT>& D, size_t hdim){return barcode(D, hdim); });\
-m.def("barcode", [](const Diagram<size_t, MT>& D, size_t hdim){return barcode(D, hdim); });\
-m.def("barcode_leftward", (std::vector<PersistencePair<size_t>> (*)(const Diagram<ReducedDGVectorSpace<MT>, MT>&, size_t))(&barcode_sparse_leftright));\
-m.def("barcode_leftward", (std::vector<PersistencePair<size_t>> (*)(const Diagram<ReducedChainComplex<MT>, MT>&, size_t))(&barcode_sparse_leftright));\
-m.def("barcode_leftward", (std::vector<PersistencePair<size_t>> (*)(const Diagram<size_t, MT>&, size_t))(&barcode_sparse_leftright));\
-m.def("barcode_rightward", (std::vector<PersistencePair<size_t>> (*)(const Diagram<ReducedDGVectorSpace<MT>, MT>&, size_t))(&barcode_sparse_rightleft));\
-m.def("barcode_rightward", (std::vector<PersistencePair<size_t>> (*)(const Diagram<ReducedChainComplex<MT>, MT>&, size_t))(&barcode_sparse_rightleft));\
-m.def("barcode_rightward", (std::vector<PersistencePair<size_t>> (*)(const Diagram<size_t, MT>&, size_t))(&barcode_sparse_rightleft));\
-m.def("barcode_dq", (std::vector<PersistencePair<size_t>> (*)(const Diagram<ReducedDGVectorSpace<MT>, MT>&, size_t))(&barcode_sparse_divide_conquer));\
-m.def("barcode_dq", (std::vector<PersistencePair<size_t>> (*)(const Diagram<ReducedChainComplex<MT>, MT>&, size_t))(&barcode_sparse_divide_conquer));\
-m.def("barcode_dq", (std::vector<PersistencePair<size_t>> (*)(const Diagram<size_t, MT>&, size_t))(&barcode_sparse_divide_conquer));
+m.def(name, [](const Diagram<ChainComplex<MT>, ChainMap<MT>> &D) {return Hom(D);});\
+m.def(name, [](const Diagram<ChainComplex<MT>, ChainMap<MT>> &D, bool topd) {return Hom(D, topd);});\
+BarcodeInterface(ReducedChainComplex<MT>, MT)\
+BarcodeInterface(ReducedDGVectorSpace<MT>, MT)\
+BarcodeInterface(int, MT)
 
 
 PYBIND11_MODULE(diagram, m) {
@@ -66,7 +73,9 @@ PYBIND11_MODULE(diagram, m) {
 	DiagramInterface(F2ChainComplex, F2ChainMap, "F2ChainDiagram")
 	DiagramInterface(F3ChainComplex, F3ChainMap, "F3ChainDiagram")
 	DiagramInterface(ReducedF2ChainComplex, M2, "F2HomDiagram")
+	DiagramInterface(ReducedF2ChainComplex, std::vector<M2>, "F2HomDiagramAll")
 	DiagramInterface(ReducedF3ChainComplex, M3, "F3HomDiagram")
+	DiagramInterface(ReducedF3ChainComplex, std::vector<M3>, "F3HomDiagramAll")
 	DiagramInterface(F2DGVectorSpace, F2DGLinearMap, "F2DGLinearDiagram")
 	DiagramInterface(F3DGVectorSpace, F3DGLinearMap, "F3DGLinearDiagram")
 	DiagramInterface(ReducedF2DGVectorSpace, M2, "F2DGHomDiagram")
@@ -88,7 +97,7 @@ PYBIND11_MODULE(diagram, m) {
     ChainFunctorInterface(M3, SimplicialComplexDiagram, "F3Chain")
 
 	DGLinearFunctorInterface(M2, SimplicialComplexDiagram, "F2DGLinearFunctor")
-	DGLinearFunctorInterface(M2, SimplicialComplexDiagram, "F2DGLinearFunctor")
+	DGLinearFunctorInterface(M2, CubicalComplexDiagram, "F2DGLinearFunctor")
 
 	AutoChainFunctorInterface(SimplicialComplexDiagram, F2)
 	AutoChainFunctorInterface(SimplicialComplexDiagram, F3)
@@ -99,6 +108,10 @@ PYBIND11_MODULE(diagram, m) {
 
 	SimpleChainFunctorInterface(m, F2)
 	SimpleChainFunctorInterface(m, F3)
+
+	FlagInterface(flags::divide_conquer, "divide_conquer")
+	FlagInterface(flags::rightward, "rightward")
+	FlagInterface(flags::leftward, "leftward")
 
 	HomFunctorInterface(M2, "Hom")
 	HomFunctorInterface(M3, "Hom")
