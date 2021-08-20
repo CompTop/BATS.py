e3421297936c9afc7b1b235d6984c4a77f7d6dfd
Brad Nelson <bradnelson@uchicago.edu>
2021-08-19 21:03:52 -0500

    pypi setup

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # BATS.py
-Python bindings for the [BATS library](https://github.com/bnels/BATS).
+
+[![PyPi](https://img.shields.io/pypi/v/bats-tda.svg)](https://pypi.org/project/bats-tda/)
+
+Python bindings for the [BATS library](https://github.com/CompTop/BATS).
 
 Note that the C++ repository is the main library, and contains more features.  This repository provides bindings for a subset of the functionality of BATS, and is under active development.
 
-Documentation, examples, and tutorials can be found at [bnels.github.io/BATS.py](https://bnels.github.io/BATS.py)
+Documentation, examples, and tutorials can be found at [comptop.github.io/BATS.py](https://comptop.github.io/BATS.py/#/)
+
+```python
+import bats
+```
+
+## Installation with pip
+
+```
+pip install bats-tda
+```
+
+Note that you need a C++17 compiler and OpenMP installed.  You can also build from source.
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # setup file to compile C++ library
-
+import setuptools
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import sys, os
-import setuptools
+
 # from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 
@@ -52,13 +52,41 @@ ext_modules = [
     ),
 ]
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name='bats',
+    name='bats-tda',
     version=__version__,
     author='Brad Nelson',
     author_email='bradnelson@uchicago.edu',
     packages=['bats','bats.visualization'],
     ext_modules=ext_modules,
-    zip_safe=False
+    zip_safe=False,
+    description="Python bindings for the Basic Applied Topology Subprograms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CompTop/BATS.py",
+    project_urls={
+        "Bug Tracker": "https://github.com/CompTop/BATS.py/issues",
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: C++',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux'
+    ],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+      ],
+    python_requires=">=3.7",
+    keywords='algebraic topology, topological data analysis, persistent homology, zigzag homology',
 )
